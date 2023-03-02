7617dc15c94a64db390d557523d80d546416185b
Anton Surkis <antonsurkis@gmail.com>
2023-03-02 18:12:42 +0300

    Solve task 7

--- hs/src/Lib.hs
+++ hs/src/Lib.hs
@@ -1,19 +1,19 @@
-module Lib {-(Tree, insert, delete, add, set, sum, reverse)-} where
+module Lib (Tree, insert, delete, add, set, sum, reverse, toList) where
 
-import           Prelude hiding (length, reverse, sum)
+import Prelude hiding (length, reverse, sum)
 
 data PendingChange = Add Int | Set Int
   deriving (Eq, Show)
 
 data Node = Node
-  { nx          :: Int,
-    nLength     :: Int,
-    nHeight     :: Int,
-    nSum        :: Int, -- Сумма до применения изменений
-    nPendRev    :: Bool,
+  { nx :: Int,
+    nLength :: Int,
+    nHeight :: Int,
+    nSum :: Int, -- Сумма до применения изменений
+    nPendRev :: Bool,
     nPendChange :: PendingChange,
-    nLeft       :: Maybe Node,
-    nRight      :: Maybe Node
+    nLeft :: Maybe Node,
+    nRight :: Maybe Node
   }
   deriving (Eq, Show)
 
@@ -28,32 +28,41 @@ instance Semigroup PendingChange where
 instance Monoid PendingChange where
   mempty = Add 0
 
+-- O(log L)
 insert :: Int -> Int -> Tree -> Tree
-insert i x t = let (tl, tr) = splitIndex i t
-                in merge tl . Just $ insertFirst x tr
+insert i x t =
+  let (tl, tr) = splitIndex i t
+   in merge tl . Just $ insertFirst x tr
 
+-- O(log L)
 delete :: Int -> Tree -> Tree
-delete i t = let (tl, tr) = splitIndex i t
-              in merge tl $ deleteFirst tr
+delete i t =
+  let (tl, tr) = splitIndex i t
+   in merge tl $ deleteFirst tr
 
+-- O(log L)
 add :: Int -> Int -> Int -> Tree -> Tree
 add l r x = change l r (Add x)
 
+-- O(log L)
 set :: Int -> Int -> Int -> Tree -> Tree
 set l r x = change l r (Set x)
 
+-- O(log L)
 sum :: Int -> Int -> Tree -> Int
 sum l r t =
   let (tlm, _) = splitIndex (r + 1) t
       (_, tm) = splitIndex l tlm
    in sumFull tm
 
+-- O(log L)
 reverse :: Int -> Int -> Tree -> Tree
 reverse l r t =
   let (tlm, tr) = splitIndex (r + 1) t
       (tl, tm) = splitIndex l tlm
    in merge (merge tl $ reverseFull tm) tr
 
+-- O(log L)
 change :: Int -> Int -> PendingChange -> Tree -> Tree
 change l r pc t =
   let (tlm, tr) = splitIndex (r + 1) t
@@ -64,8 +73,9 @@ change l r pc t =
 merge :: Tree -> Tree -> Tree
 merge Nothing t = t
 merge t Nothing = t
-merge tl (Just nr) = let (x, tr) = extractFirst nr
-                      in Just $ node x tl tr
+merge tl (Just nr) =
+  let (x, tr) = extractFirst nr
+   in Just $ node x tl tr
 
 -- O(log L)
 splitIndex :: Int -> Tree -> (Tree, Tree)
@@ -74,66 +84,120 @@ splitIndex i (Just n')
   | i <= 0 = (Nothing, Just n)
   | i >= nLength n = (Just n, Nothing)
   | i <= length (nLeft n) =
-    let (l, r) = splitIndex i $ nLeft n
-     in (l, Just . rebalance $ node (nx n) r (nRight n))
+      let (l, r) = splitIndex i $ nLeft n
+       in (l, Just . rebalance $ node (nx n) r (nRight n))
   | otherwise =
-    let (l, r) = splitIndex (i - length (nLeft n) - 1) $ nRight n
-     in (Just . rebalance $ node (nx n) (nLeft n) l, r)
+      let (l, r) = splitIndex (i - length (nLeft n) - 1) $ nRight n
+       in (Just . rebalance $ node (nx n) (nLeft n) l, r)
   where
     n = normalize n'
 
 -- O(log L)
 insertFirst :: Int -> Tree -> Node
 insertFirst x Nothing = node x Nothing Nothing
-insertFirst x (Just n') = let n = normalize n' in rebalance $
-  node (nx n) (Just . insertFirst x $ nLeft n) (nRight n)
+insertFirst x (Just n') =
+  let n = normalize n'
+   in rebalance $
+        node (nx n) (Just . insertFirst x $ nLeft n) (nRight n)
 
 -- O(log L)
 deleteFirst :: Tree -> Tree
-deleteFirst Nothing  = Nothing
+deleteFirst Nothing = Nothing
 deleteFirst (Just n) = snd $ extractFirst n
 
 -- O(log L)
 extractFirst :: Node -> (Int, Tree)
-extractFirst n' = let n = normalize n' in case nLeft n of
-  Nothing -> (nx n, nRight n)
-  Just n'' -> (\t -> Just . rebalance $ node (nx n) t (nRight n)) <$> extractFirst n''
+extractFirst n' =
+  let n = normalize n'
+   in case nLeft n of
+        Nothing -> (nx n, nRight n)
+        Just n'' -> (\t -> Just . rebalance $ node (nx n) t (nRight n)) <$> extractFirst n''
 
 -- O(1)
 reverseFull :: Tree -> Tree
-reverseFull = fmap $ \n -> Node (nx n) (nLength n) (nHeight n)
-  (nSum n) (not $ nPendRev n) (nPendChange n) (nLeft n) (nRight n)
+reverseFull = fmap $ \n ->
+  Node
+    (nx n)
+    (nLength n)
+    (nHeight n)
+    (nSum n)
+    (not $ nPendRev n)
+    (nPendChange n)
+    (nLeft n)
+    (nRight n)
 
 -- O(1)
 changeFull :: PendingChange -> Tree -> Tree
-changeFull pc = fmap $ \n -> Node (nx n) (nLength n) (nHeight n)
-  (nSum n) (nPendRev n) (pc <> nPendChange n) (nLeft n) (nRight n)
+changeFull pc = fmap $ \n ->
+  Node
+    (nx n)
+    (nLength n)
+    (nHeight n)
+    (nSum n)
+    (nPendRev n)
+    (pc <> nPendChange n)
+    (nLeft n)
+    (nRight n)
 
+-- Шаг ребалансировки АВЛ-дерева.
+-- Наиболее проклятая часть решения.
 rebalance :: Node -> Node
-rebalance = id -- undefined
+rebalance n
+  | height (nLeft n) > height (nRight n) + 1 = case nLeft n of
+      Just nl' ->
+        let nl = normalize nl'
+         in if height (nRight nl) <= height (nRight n)
+              then node (nx nl) (nLeft nl) (Just $ node (nx n) (nRight nl) (nRight n))
+              else case nRight nl of
+                Just nlr' ->
+                  let nlr = normalize nlr'
+                   in node
+                        (nx nlr)
+                        (Just $ node (nx nl) (nLeft nl) (nLeft nlr))
+                        (Just $ node (nx n) (nRight nlr) (nRight n))
+                Nothing -> error "Impossible"
+      Nothing -> error "Impossible"
+  | height (nLeft n) + 1 < height (nRight n) = case nRight n of
+      Just nr' ->
+        let nr = normalize nr'
+         in if height (nLeft nr) <= height (nRight nr)
+              then node (nx nr) (Just $ node (nx n) (nLeft n) (nLeft nr)) (nRight nr)
+              else case nLeft nr of
+                Just nrl' ->
+                  let nrl = normalize nrl'
+                   in node
+                        (nx nrl)
+                        (Just $ node (nx n) (nLeft n) (nLeft nrl))
+                        (Just $ node (nx nr) (nRight nrl) (nRight nr))
+                Nothing -> error "Impossible"
+      Nothing -> error "Impossible"
+  | otherwise = n
 
 -- O(1)
 normalize :: Node -> Node
-normalize = normalizeRev . normalizeChange
+normalize n
+  | nPendRev n = node (nx n) reversedRight reversedLeft
+  | otherwise = node (nx n) changedLeft changedRight
   where
-    normalizeRev n
-      | nPendRev n = node (nx n) (reverseFull $ nRight n) (reverseFull $ nLeft n)
-      | otherwise = n
-
-    normalizeChange n =
-      let pc = nPendChange n
-       in node (nx n)
-          (changeFull pc $ nLeft n)
-          (changeFull pc $ nRight n)
+    changedLeft = changeFull (nPendChange n) (nLeft n)
+    changedRight = changeFull (nPendChange n) (nRight n)
+    reversedLeft = reverseFull changedLeft
+    reversedRight = reverseFull changedRight
 
 node :: Int -> Tree -> Tree -> Node
 node x left right =
-  Node x
-  (1 + length left + length right)
-  (1 + max (height left) (height right))
-  (x + sumFull left + sumFull right)
-  False mempty left right
+  Node
+    x
+    (1 + length left + length right)
+    (1 + max (height left) (height right))
+    (x + sumFull left + sumFull right)
+    False
+    mempty
+    left
+    right
 
+-- Инвариант: если спустились в вершину,
+-- то эта сумма корректна.
 sumFull :: Tree -> Int
 sumFull Nothing = 0
 sumFull (Just n) = case nPendChange n of
@@ -146,6 +210,9 @@ length = maybe 0 nLength
 height :: Tree -> Int
 height = maybe 0 nHeight
 
-linearize :: Tree -> [Int]
-linearize Nothing  = []
-linearize (Just n) = linearize (nLeft n) ++ nx n : linearize (nRight n)
+toList :: Tree -> [Int]
+toList Nothing = []
+
+linearize (Just n') =
+  let n = normalize n'
+   in linearize (nLeft n) ++ nx n : linearize (nRight n)
